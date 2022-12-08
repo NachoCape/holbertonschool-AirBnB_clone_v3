0408d95c60ba2e579a3c6c10ce0522b94649ece8
Damian25121997 <damiancaetano25@gmail.com>
2022-12-08 14:19:02 -0300

    Add comment document and fix pycode

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,9 +9,11 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close(exe):
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""""__init__ module"""
 from flask import Blueprint
 
 
