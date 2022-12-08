16986aa65212f71b78e3a8962b576244c2151dc0
Damian25121997 <damiancaetano25@gmail.com>
2022-12-08 14:27:26 -0300

    fix return of index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """index module"""
 from api.v1.views import app_views
-from flask import jsonify
 
 
 @app_views.route('/status', strict_slashes=False)
 def return_status():
-    return jsonify({"status": "OK"})
+    return ({"status": "OK"})
