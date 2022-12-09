052914ed3771a7edbe9093cf68ea4a6c8581f284
Damian <damiancaetano25@gmail.com>
2022-12-09 00:45:34 +0000

    Add in app.py environment variable in app.run

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,4 +16,4 @@ def close(exe):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), port=int(getenv("HBNB_API_PORT", "5000")), threaded=True, debug=True)
